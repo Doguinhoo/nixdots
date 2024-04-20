d7d75f75ece92a8a93eafa1b60760a1c83b1649d
lucas <patruop@gmail.com>
2024-04-20 00:27:30 -0300

    hardware conf

--- hosts/lucas/hardware-configuration.nix
+++ hosts/lucas/hardware-configuration.nix
@@ -12,30 +12,38 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" "sr_mod" "rtsx_usb_sdmmc"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
+  boot.kernel.sysctl."vm.max_map_count" = 2147483642;
+
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/ed3fdfdd-8859-489f-a868-7ea460ac2eda";
+    device = "/dev/disk/by-uuid/11883963-54e7-4716-89be-db813788823c";
     fsType = "ext4";
   };
 
+  boot.initrd.luks.devices."crypted".device = "/dev/disk/by-uuid/94069ec8-f23c-4aac-a313-4a9c4bcf1398";
+
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/521D-F4B4";
+    device = "/dev/disk/by-uuid/9CED-139E";
     fsType = "vfat";
   };
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 32 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
